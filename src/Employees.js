775d6bd7dba7d34b506a02b964229f87cfb9c1a6
unknown <drakewang@gmail.com>
2022-09-15 15:07:22 -0700

    Add select team feature

--- src/Employees.js
+++ src/Employees.js
@@ -3,6 +3,9 @@ import femaleProfile from './images/femaleProfile.jpg'
 import maleProfile from './images/maleProfile.jpg'
 
 const Employees = () => {
+
+	const [selectedTeam, setTeam] = useState("TeamB")
+
 	const [employees, setEmployees] = useState([
 	{
 	    id: 1,
@@ -89,14 +92,36 @@ const Employees = () => {
 	    teamName: "TeamD"
 	  }
 	])
+
+	function handleTeamSelectionChange (event) {
+		setTeam(event.target.value)
+	}
+
+	function handleEmployeeCardClick (event) {
+		const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+													?(employee.teamName === selectedTeam)?{...employee, teamName:''}:{...employee,teamName: selectedTeam}
+													:employee);
+		setEmployees(transformedEmployees)
+	}
+
 	return (
 		<main className="container">
+			<div className="row justify-content-center mt-3 mb-3">
+				<div className="col-8">
+					<select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
+						<option value="TeamA">TeamA</option>
+						<option value="TeamB">TeamB</option>
+						<option value="TeamC">TeamC</option>
+						<option value="TeamD">TeamD</option>
+					</select>
+				</div>
+			</div>
 			<div className="row justify-content-center mt-3 mb-3">
 				<div className="col-8">
 					<div className="card-collection">
 						{
 							employees.map((employee) => (
-								<div id={employee.id} className="card m-2" style={{ cursor: "pointer" }}>
+								<div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
 
 									{(employee.gender === "female") ?<img src={femaleProfile} className="card-img-top"/>
                                                                     :<img src={maleProfile} className="card-img-top"/>}
