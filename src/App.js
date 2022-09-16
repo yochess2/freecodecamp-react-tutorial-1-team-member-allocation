e1441090da93f2e490f395c11019832adb1227c9
unknown <drakewang@gmail.com>
2022-09-15 19:36:43 -0700

    Add router and navbar, 123:45

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css'
 import Header from './Header'
 import Employees from './Employees'
 import Footer from './Footer'
+import GroupedTeamMembers from './GroupedTeamMembers'
+import Nav from './Nav'
+import NotFound from './NotFound'
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
@@ -117,15 +122,24 @@ function App() {
   }
 
   return (
-    <div>
+    <Router>
+      <Nav/>
       <Header selectedTeam={selectedTeam}
         teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length} />
-      <Employees employees={employees}
-        selectedTeam={selectedTeam}
-        handleEmployeeCardClick={handleEmployeeCardClick}
-        handleTeamSelectionChange={handleTeamSelectionChange}/>
+      <Routes>
+        <Route path="/"
+               element={<Employees employees={employees}
+            selectedTeam={selectedTeam}
+            handleEmployeeCardClick={handleEmployeeCardClick}
+            handleTeamSelectionChange={handleTeamSelectionChange}/>}>
+        </Route>
+        <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers/>}>
+        </Route>
+        <Route path="*" element={<NotFound />}>
+        </Route>
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/Employees.js
+++ src/Employees.js
@@ -21,7 +21,7 @@ const Employees = ({ employees, selectedTeam, handleEmployeeCardClick, handleTea
 					<div className="card-collection">
 						{
 							employees.map((employee) => (
-								<div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
+								<div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
 
 									{(employee.gender === "female") ?<img src={femaleProfile} className="card-img-top"/>
                                                                     :<img src={maleProfile} className="card-img-top"/>}
